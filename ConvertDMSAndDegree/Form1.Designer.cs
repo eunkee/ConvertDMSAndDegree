3a421dc12b011e5a69a168e15dce07398619ea31
eunkee <axlovelee@naver.com>
2021-12-17 10:14:39 +0900

    초기 커밋.

---
+++ ConvertDMSAndDegree/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace ConvertDMSAndDegree
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLat1 = new System.Windows.Forms.TextBox();
+            this.textBoxLat2 = new System.Windows.Forms.TextBox();
+            this.textBoxLat3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxLon3 = new System.Windows.Forms.TextBox();
+            this.textBoxLon2 = new System.Windows.Forms.TextBox();
+            this.textBoxLon1 = new System.Windows.Forms.TextBox();
+            this.buttonDMStoDegree = new System.Windows.Forms.Button();
+            this.buttonDegreetoDMS = new System.Windows.Forms.Button();
+            this.buttonClearDMS = new System.Windows.Forms.Button();
+            this.buttonClearDegree = new System.Windows.Forms.Button();
+            this.TextBoxDegreeLat = new System.Windows.Forms.TextBox();
+            this.TextBoxDegreeLon = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxLat1
+            // 
+            this.textBoxLat1.Location = new System.Drawing.Point(32, 57);
+            this.textBoxLat1.Name = "textBoxLat1";
+            this.textBoxLat1.Size = new System.Drawing.Size(77, 23);
+            this.textBoxLat1.TabIndex = 0;
+            // 
+            // textBoxLat2
+            // 
+            this.textBoxLat2.Location = new System.Drawing.Point(128, 57);
+            this.textBoxLat2.Name = "textBoxLat2";
+            this.textBoxLat2.Size = new System.Drawing.Size(77, 23);
+            this.textBoxLat2.TabIndex = 1;
+            // 
+            // textBoxLat3
+            // 
+            this.textBoxLat3.Location = new System.Drawing.Point(224, 57);
+            this.textBoxLat3.Name = "textBoxLat3";
+            this.textBoxLat3.Size = new System.Drawing.Size(77, 23);
+            this.textBoxLat3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(110, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "°";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(206, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "\'";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(304, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "\"";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(304, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 21);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "\"";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(206, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 21);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "\'";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(110, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 21);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "°";
+            // 
+            // textBoxLon3
+            // 
+            this.textBoxLon3.Location = new System.Drawing.Point(224, 97);
+            this.textBoxLon3.Name = "textBoxLon3";
+            this.textBoxLon3.Size = new System.Drawing.Size(77, 23);
+            this.textBoxLon3.TabIndex = 11;
+            // 
+            // textBoxLon2
+            // 
+            this.textBoxLon2.Location = new System.Drawing.Point(128, 97);
+            this.textBoxLon2.Name = "textBoxLon2";
+            this.textBoxLon2.Size = new System.Drawing.Size(77, 23);
+            this.textBoxLon2.TabIndex = 10;
+            // 
+            // textBoxLon1
+            // 
+            this.textBoxLon1.Location = new System.Drawing.Point(32, 97);
+            this.textBoxLon1.Name = "textBoxLon1";
+            this.textBoxLon1.Size = new System.Drawing.Size(77, 23);
+            this.textBoxLon1.TabIndex = 9;
+            // 
+            // buttonDMStoDegree
+            // 
+            this.buttonDMStoDegree.Location = new System.Drawing.Point(353, 58);
+            this.buttonDMStoDegree.Name = "buttonDMStoDegree";
+            this.buttonDMStoDegree.Size = new System.Drawing.Size(51, 23);
+            this.buttonDMStoDegree.TabIndex = 15;
+            this.buttonDMStoDegree.Text = " -> ";
+            this.buttonDMStoDegree.UseVisualStyleBackColor = true;
+            this.buttonDMStoDegree.Click += new System.EventHandler(this.ButtonDMStoDegree_Click);
+            // 
+            // buttonDegreetoDMS
+            // 
+            this.buttonDegreetoDMS.Location = new System.Drawing.Point(353, 95);
+            this.buttonDegreetoDMS.Name = "buttonDegreetoDMS";
+            this.buttonDegreetoDMS.Size = new System.Drawing.Size(51, 23);
+            this.buttonDegreetoDMS.TabIndex = 16;
+            this.buttonDegreetoDMS.Text = " <- ";
+            this.buttonDegreetoDMS.UseVisualStyleBackColor = true;
+            this.buttonDegreetoDMS.Click += new System.EventHandler(this.ButtonDegreetoDMS_Click);
+            // 
+            // buttonClearDMS
+            // 
+            this.buttonClearDMS.Location = new System.Drawing.Point(88, 15);
+            this.buttonClearDMS.Name = "buttonClearDMS";
+            this.buttonClearDMS.Size = new System.Drawing.Size(75, 23);
+            this.buttonClearDMS.TabIndex = 17;
+            this.buttonClearDMS.Text = "Clear";
+            this.buttonClearDMS.UseVisualStyleBackColor = true;
+            this.buttonClearDMS.Click += new System.EventHandler(this.ButtonClearDMS_Click);
+            // 
+            // buttonClearDegree
+            // 
+            this.buttonClearDegree.Location = new System.Drawing.Point(526, 15);
+            this.buttonClearDegree.Name = "buttonClearDegree";
+            this.buttonClearDegree.Size = new System.Drawing.Size(75, 23);
+            this.buttonClearDegree.TabIndex = 18;
+            this.buttonClearDegree.Text = "Clear";
+            this.buttonClearDegree.UseVisualStyleBackColor = true;
+            this.buttonClearDegree.Click += new System.EventHandler(this.ButtonClearDegree_Click);
+            // 
+            // TextBoxDegreeLat
+            // 
+            this.TextBoxDegreeLat.Location = new System.Drawing.Point(454, 55);
+            this.TextBoxDegreeLat.Name = "TextBoxDegreeLat";
+            this.TextBoxDegreeLat.Size = new System.Drawing.Size(196, 23);
+            this.TextBoxDegreeLat.TabIndex = 19;
+            // 
+            // TextBoxDegreeLon
+            // 
+            this.TextBoxDegreeLon.Location = new System.Drawing.Point(454, 95);
+            this.TextBoxDegreeLon.Name = "TextBoxDegreeLon";
+            this.TextBoxDegreeLon.Size = new System.Drawing.Size(196, 23);
+            this.TextBoxDegreeLon.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(24, 14);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 21);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "DMS";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(445, 14);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 21);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Degree";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(671, 136);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.TextBoxDegreeLon);
+            this.Controls.Add(this.TextBoxDegreeLat);
+            this.Controls.Add(this.buttonClearDegree);
+            this.Controls.Add(this.buttonClearDMS);
+            this.Controls.Add(this.buttonDegreetoDMS);
+            this.Controls.Add(this.buttonDMStoDegree);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxLon3);
+            this.Controls.Add(this.textBoxLon2);
+            this.Controls.Add(this.textBoxLon1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxLat3);
+            this.Controls.Add(this.textBoxLat2);
+            this.Controls.Add(this.textBoxLat1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Convert DMS <-> Degree";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLat1;
+        private System.Windows.Forms.TextBox textBoxLat2;
+        private System.Windows.Forms.TextBox textBoxLat3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxLon3;
+        private System.Windows.Forms.TextBox textBoxLon2;
+        private System.Windows.Forms.TextBox textBoxLon1;
+        private System.Windows.Forms.Button buttonDMStoDegree;
+        private System.Windows.Forms.Button buttonDegreetoDMS;
+        private System.Windows.Forms.Button buttonClearDMS;
+        private System.Windows.Forms.Button buttonClearDegree;
+        private System.Windows.Forms.TextBox TextBoxDegreeLat;
+        private System.Windows.Forms.TextBox TextBoxDegreeLon;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ ConvertDMSAndDegree/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConvertDMSAndDegree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //십진법 -> 도,분,초
+        private void ConvertDecdegToDMS(double angle, out int decdeg, out int minsec, out double sec)
+        {
+            double t_decdeg = angle;
+            double t_minsec = (t_decdeg - Math.Truncate(t_decdeg)) * 60;
+            double t_sec = (t_minsec - Math.Truncate(t_minsec)) * 60;
+
+            decdeg = (int)Math.Truncate(t_decdeg);
+            minsec = (int)Math.Truncate(t_minsec);
+            sec = Math.Round(t_sec, 2);
+        }
+
+        //도,분,초 -> 십진법
+        private void ConvertDMSToDecdeg(int decdeg, int minsec, double sec, out double DMS)
+        {
+            double t_angle = (double)decdeg + (double)minsec / 60 + sec / 3600;
+            DMS = Math.Round(t_angle, 5);
+        }
+
+        private void ButtonDMStoDegree_Click(object sender, EventArgs e)
+        {
+            string textDMSLat1 = textBoxLat1.Text;
+            string textDMSLat2 = textBoxLat2.Text;
+            string textDMSLat3 = textBoxLat3.Text;
+            string textDMSLon1 = textBoxLon1.Text;
+            string textDMSLon2 = textBoxLon2.Text;
+            string textDMSLon3 = textBoxLon3.Text;
+
+            if (textDMSLat1.Length > 0 && textDMSLat2.Length > 0 && textDMSLat3.Length > 0)
+            {
+                if (int.TryParse(textDMSLat1, out int dmsLat1)
+                    && int.TryParse(textDMSLat1, out int dmsLat2)
+                    && int.TryParse(textDMSLat1, out int dmsLat3))
+                {
+                    ConvertDMSToDecdeg(dmsLat1, dmsLat2, dmsLat3, out double Lat);
+                    TextBoxDegreeLat.Text = Lat.ToString();
+                }
+            }
+
+            if (textDMSLon1.Length > 0 && textDMSLon2.Length > 0 && textDMSLon3.Length > 0)
+            {
+                if (int.TryParse(textDMSLon1, out int dmsLon1)
+                    && int.TryParse(textDMSLon2, out int dmsLon2)
+                    && int.TryParse(textDMSLon3, out int dmsLon3))
+                {
+                    ConvertDMSToDecdeg(dmsLon1, dmsLon2, dmsLon3, out double Lat);
+                    TextBoxDegreeLat.Text = Lat.ToString();
+                }
+            }
+        }
+
+        private void ButtonDegreetoDMS_Click(object sender, EventArgs e)
+        {
+            string textDegreeLat = TextBoxDegreeLat.Text;
+            string textDegreeLon = TextBoxDegreeLon.Text;
+
+            if (textDegreeLat.Length > 0)
+            {
+                if (double.TryParse(textDegreeLat, out double degreeLat))
+                {
+                    ConvertDecdegToDMS(degreeLat, out int Latdecdeg, out int Latminsec, out double Latsec);
+                    textBoxLat1.Text = Latdecdeg.ToString();
+                    textBoxLat2.Text = Latminsec.ToString();
+                    textBoxLat3.Text = Latsec.ToString();
+                }
+            }
+
+            if (textDegreeLon.Length > 0)
+            {
+                if (double.TryParse(textDegreeLon, out double degreeLon))
+                {
+                    ConvertDecdegToDMS(degreeLon, out int Londecdeg, out int Lonminsec, out double Lonsec);
+                    textBoxLon1.Text = Londecdeg.ToString();
+                    textBoxLon2.Text = Lonminsec.ToString();
+                    textBoxLon3.Text = Lonsec.ToString();
+                }
+            }
+        }
+
+        private void ButtonClearDMS_Click(object sender, EventArgs e)
+        {
+            textBoxLat1.Text = string.Empty;
+            textBoxLat2.Text = string.Empty;
+            textBoxLat3.Text = string.Empty;
+            textBoxLon1.Text = string.Empty;
+            textBoxLon2.Text = string.Empty;
+            textBoxLon3.Text = string.Empty;
+        }
+
+        private void ButtonClearDegree_Click(object sender, EventArgs e)
+        {
+            TextBoxDegreeLat.Text = string.Empty;
+            TextBoxDegreeLon.Text = string.Empty;
+        }
+    }
+}
